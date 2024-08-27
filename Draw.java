98a91a55a56b2cf0326f6cd38ed14e30e4dd3d6c
gifro <viataliifedyn@gmail.com>
2024-08-27 08:30:25 -0500

    first commit

---
+++ Draw.java
@@ -0,0 +1,11 @@
+import java.awt.Window;
+import java.awt.image.renderable.*;
+
+public class Draw {
+    public static void main(String[] args) {
+        System.out.println("running servers");
+
+        Window w = new Window(Window owner, GraphicsConfiguration gc);
+        Rectangle bounds = gc.getBounds();
+    }
+}
